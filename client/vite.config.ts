dd31961b2daa0d671f7165a1323807e8ec40670b
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-03-17 21:15:19 +0300

    use icon configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Icons({
+      compiler: 'jsx',
+      jsx: 'react',
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
@@ -11,5 +18,5 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
-  }
-})
+  },
+});
